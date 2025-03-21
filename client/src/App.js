127738631e942016afdb54477d60d527ef99ae5f
Aman <amanshrestha016@gmail.com>
2025-03-21 19:16:43 +0545

    feat: added backend for user, agent, and admin authentication\n\n- Implemented sign-up and login for users, agents, and admins\n- Added password encoding and decoding\n- Enabled selection of all users and agents from the database

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Login from "./user/Login";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
